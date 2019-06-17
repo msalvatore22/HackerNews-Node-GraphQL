356986265f8c4018a9fc918d6a0973732a0157fe
Michael Salvatore <msalvatore22@gmail.com>
2019-06-16 20:48:38 -0400

    added prisma client for CRUD operations in resolvers, generated demo database in prisma cloud

--- src/index.js
+++ src/index.js
@@ -1,56 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      const link = links.find(link => link.id === args.id)
-      return link
-    }
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
+        description: args.description,
+      })
     },
-    updateLink: (_,{id, description, url}) => {
-      const link = links.find(link => link.id === id)
-      if(!link){
-        throw new Error(`Couldn't find link with id ${id}`)
-      }
-      if(description !== undefined){
-        link.description = description
-      }
-      if(url !== undefined){
-        link.url = url
-      }
-      return link
-    },
-    deleteLink: (parent, args) => {
-      const removeIndex = links.map(function(args){ return args.id }).indexOf(args.id)
-      links.splice(removeIndex, 1)
-      return links[removeIndex]
-    }
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
